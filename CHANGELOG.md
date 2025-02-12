4165f34b27c8050f2003508d828103cc1c25174a
Ben Houston <neuralsoft@gmail.com>
2025-02-12 11:26:47 -0500

    address windows bug in dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # mycoder
 
+## 0.0.23
+
+### Patch Changes
+
+- upgrade dependency to fix windows bug
+
 ## 0.0.22
 
 ### Patch Changes
--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,2 @@
 #!/usr/bin/env node
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import { join } from "path";
-
-const cliUrl = import.meta.url;
-const distIndex = cliUrl.replace("bin/cli.js", "dist/index.js");
-
-await import(distIndex);
+import "../dist/index.js";
